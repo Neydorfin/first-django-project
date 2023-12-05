5b2636f7006f02aebe981d1ca07386d976ce6fc4
Иван Галинский <iongalin.02@gmail.com>
2023-12-05 20:37:09 +0300

    add template

--- myPage/shopapp/views.py
+++ myPage/shopapp/views.py
@@ -1,6 +1,11 @@
+import random
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
 def shop_index(request: HttpRequest) -> HttpResponse:
-    return render()
+    context = {
+        'list': [random.randint(x, 50) for x in range(9)],
+        'name': 'blanditiis',
+    }
+    return render(request, 'shopapp/index.html', context=context)
