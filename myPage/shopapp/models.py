f70fe64cb4f34e2a5fd30ccde4974004d8bf5b8a
Иван Галинский <iongalin.02@gmail.com>
2023-12-11 20:17:10 +0300

    models of products and orders maked

--- myPage/shopapp/models.py
+++ myPage/shopapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    discount = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    archived = models.BooleanField(default=False)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    delivery_address = models.TextField(blank=True)
+    promocode = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product ,related_name="orders")
