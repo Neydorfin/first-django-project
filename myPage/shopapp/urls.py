843e4a5396446c1e0b01f423916c587839f0cb81
Иван Галинский <iongalin.02@gmail.com>
2023-12-25 16:06:12 +0300

    urls updated

--- myPage/shopapp/urls.py
+++ myPage/shopapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import shop_index, product_page, order_page, upload_file
+from .views import create_product, shop_index, product_page, order_page, upload_file, create_order
 
 app_name = "shopapp"
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path("products", product_page, name="products"),
     path("orders", order_page, name="orders"),
     path("upload", upload_file, name="upload"),
+    path("products/create", create_product, name="create_product"),
+    path("orders/create", create_order, name="create_order"),
 ]
