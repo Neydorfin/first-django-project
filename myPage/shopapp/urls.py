35c204d2284cc1f0a7a341a14934f05c596dacf6
Иван Галинский <iongalin.02@gmail.com>
2024-01-15 17:14:33 +0300

    created product generic classes

--- myPage/shopapp/urls.py
+++ myPage/shopapp/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
-from .views import create_product, shop_index, product_page, order_page, upload_file, create_order
+from .views import (
+    ShopIndex, 
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductArchivedView,
+    order_page, 
+    upload_file, 
+    create_order)
 
 app_name = "shopapp"
 urlpatterns = [
-    path("", shop_index, name="index"),
-    path("products", product_page, name="products"),
-    path("orders", order_page, name="orders"),
+    path("", ShopIndex.as_view(), name="index_shop"),
+    path("products", ProductListView.as_view(), name="product_list"),
+    path("products/create", ProductCreateView.as_view(), name="product_create"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("products/<int:pk>/update", ProductUpdateView.as_view(), name="product_update"),
+    path("products/<int:pk>/archived", ProductArchivedView.as_view(), name="product_archived"),
+
+
+    path("orders", order_page, name="order_list"),
+    path("orders/create", create_order, name="order_create"),
+
     path("upload", upload_file, name="upload"),
-    path("products/create", create_product, name="create_product"),
-    path("orders/create", create_order, name="create_order"),
+
 ]
--- myPage/shopapp/views.py
+++ myPage/shopapp/views.py
@@ -1,63 +1,78 @@
-import random
 from django.shortcuts import redirect, render
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.utils import archive
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from shopapp.forms import OrderForm, ProductForm
 
 from shopapp.models import Order, Product
 
 
-def shop_index(request: HttpRequest) -> HttpResponse:
-    context = {
-        'list': [random.randint(x, 50) for x in range(9)],
-        'name': 'blanditiis',
-    }
-    return render(request, 'shopapp/index.html', context=context)
+class ShopIndex(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'shopapp/index.html')
 
 
-def product_page(request: HttpRequest) -> HttpResponse:
-    context = {
-        "products" : Product.objects.all()
-    }
-    return render(request, 'shopapp/product-list.html', context=context)
+class ProductListView(ListView):
+    queryset = Product.objects.filter(archived=False)
+    template_name = "shopapp/product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "shopapp/product_detail.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = "shopapp/product_create.html"
+    form_class = ProductForm
+    success_url = reverse_lazy("shopapp:products_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "shopapp/product_update.html"
+    form_class = ProductForm
+    context_object_name = "product"
+
+    def get_success_url(self) -> str:
+        return reverse(
+            "shopapp:product_detail",
+            kwargs={"pk": self.object.pk}
+        )
+
+
+class ProductArchivedView(DeleteView):
+    model = Product
+    template_name = "shopapp/product_archived.html"
+    context_object_name = "product"
+
+    def get_success_url(self) -> str:
+        return reverse(
+            "shopapp:product_detail",
+            kwargs={"pk": self.object.pk}
+        )
+
+    def form_valid(self, form):
+        success_url = self.get_success_url()
+        if self.object.archived:
+            self.object.archived = False
+        else:
+            self.object.archived = True
+        self.object.save()
+        return HttpResponseRedirect(success_url)
 
 
 def order_page(request: HttpRequest) -> HttpResponse:
     context = {
-        "orders" : Order.objects.select_related("user").prefetch_related("products").all()
+        "orders": Order.objects.select_related("user").prefetch_related("products").all()
     }
-    return render(request, 'shopapp/order-list.html', context=context)
-
-
-def upload_file(request: HttpRequest) -> HttpResponse:
-    context = {}
-    if request.method == "POST" and request.FILES.get('isFile'):
-        file = request.FILES.get('isFile')
-        context["name"] = file.name
-        if file.size < 1_000_000:
-            context["size"] = file.size
-            fs = FileSystemStorage()
-            filename = fs.save(file.name, file)
-        else:
-            context["size"] = "error"
-    
-    return render(request, "shopapp/upload-file.html", context=context)
-
-
-def create_product(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            url = reverse("shopapp:products")
-            return redirect(url)
-    else:
-        form = ProductForm()
-    context = {
-        "form": form
-    }
-    return render(request, "shopapp/create-product.html", context=context)
+    return render(request, 'shopapp/order_list.html', context=context)
 
 
 def create_order(request: HttpRequest) -> HttpResponse:
@@ -73,4 +88,19 @@ def create_order(request: HttpRequest) -> HttpResponse:
     context = {
         "form": form
     }
-    return render(request, "shopapp/create-order.html", context=context)
+    return render(request, "shopapp/order_create.html", context=context)
+
+
+def upload_file(request: HttpRequest) -> HttpResponse:
+    context = {}
+    if request.method == "POST" and request.FILES.get('isFile'):
+        file = request.FILES.get('isFile')
+        context["name"] = file.name
+        if file.size < 1_000_000:
+            context["size"] = file.size
+            fs = FileSystemStorage()
+            filename = fs.save(file.name, file)
+        else:
+            context["size"] = "error"
+
+    return render(request, "shopapp/upload_file.html", context=context)
