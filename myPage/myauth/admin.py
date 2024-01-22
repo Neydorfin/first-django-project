699b76d8c4697ee9d4e465892a4faa1c3aa6cfdd
Иван Галинский <iongalin.02@gmail.com>
2024-01-22 14:59:00 +0300

    profile added to admin panel

--- myPage/myauth/admin.py
+++ myPage/myauth/admin.py
@@ -1,3 +1,30 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "user_name",
+        "bio_short",
+        "agreement_accepted",
+    )
+    list_display_links = (
+        "pk",
+        "user_name",
+    )
+
+    def bio_short(self, obj: Profile) -> str:
+        if len(obj.bio) > 48:
+            return obj.bio[:48] + "..."
+        return obj.bio
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return Profile.objects.select_related("user")
+
+    def user_name(self, obj: Profile) -> str:
+        return obj.user.first_name or obj.user.username
