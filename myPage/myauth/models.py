4d7c7d94756566a05caa1bc9d08cbad937ecbbbb
Иван Галинский <iongalin.02@gmail.com>
2024-01-29 17:49:52 +0300

    avatar added to profile

--- myPage/myauth/models.py
+++ myPage/myauth/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def avatar_directory_path(instance: "Profile", filename: str) -> str:
+    return "profile/{user_pk}/avatar/{filename}".format(
+        user_pk=instance.user.pk,
+        filename=filename,
+    )
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     agreement_accepted = models.BooleanField(default=False)
+    avatar = models.ImageField(null=True, blank=True, upload_to=avatar_directory_path)
