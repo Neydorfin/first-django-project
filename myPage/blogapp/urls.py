74b02ce2bee628480a3083b2eeac6979a88b1793
Иван Галинский <iongalin.02@gmail.com>
2024-02-17 17:06:01 +0300

    article list view created

--- myPage/blogapp/urls.py
+++ myPage/blogapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from blogapp.views import ArticleListView
 
 app_name = "blog"
 urlpatterns = [
-   
+   path("", ArticleListView.as_view(), name="articles_list")
 ]
--- myPage/blogapp/views.py
+++ myPage/blogapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
+from blogapp.models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = "blogapp/article_list.html"
+    context_object_name = 'articles'
