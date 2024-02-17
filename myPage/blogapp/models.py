02817bcbb41087e58ea0500d8e1e76ed47e9dbf3
Иван Галинский <iongalin.02@gmail.com>
2024-02-17 16:44:14 +0300

    created category model

--- myPage/blogapp/models.py
+++ myPage/blogapp/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(null=True, blank=True)
-    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
