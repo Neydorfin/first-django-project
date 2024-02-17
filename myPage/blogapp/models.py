c0bc1da233e4dcaa4f927f4fb707fd2ea5df8307
Иван Галинский <iongalin.02@gmail.com>
2024-02-17 16:53:27 +0300

    created article model

--- myPage/blogapp/models.py
+++ myPage/blogapp/models.py
@@ -12,3 +12,12 @@ class Category(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=20)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
